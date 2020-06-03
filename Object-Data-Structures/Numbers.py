bd0f85e7bcfd729c413a3ad02caf567eb0147d54
Carl Redding <credding07@gmail.com>
2020-06-03 13:28:29 -0500

    created folder for Object-Data-Structures in python. numbers.py created

---
+++ Object-Data-Structures/Numbers.py
@@ -0,0 +1,34 @@
+'''
+Two main types of numbers we will be working with in python
+- Integers which are whole numbers.
+- Floating Point numbers which are numbers with a decimal
+Sneaking how to create variables and assign them values as well.
+'''
+
+#Basic math
+print("///// basic math")
+print(2+1) # 3
+print(2-1) # 1
+print(2*2) # 4
+print(3/2) # 1.5
+
+# Modulo or "Mod" Operator
+print('/////modulo returns the remainder after division')
+print(7/4) #1.75
+print(7%4) #3
+print(50%5) #0
+
+# checking if even or od
+print("///// even or odd")
+print(20%2)
+print(21%2)
+
+# to the power 
+print("///// to the power")
+print(2 ** 3)
+
+# equations
+print("///// equations PEDMAS")
+print(2 + 10 * 10 + 3)
+print((2 + 10) * (10 + 3))
+
