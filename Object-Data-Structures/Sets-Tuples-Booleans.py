888603dcda9f29ae0701b809a679a9f4e583a569
Carl Redding <credding07@gmail.com>
2020-06-07 22:52:43 -0400

    sets have been added to the file and covered with examples and a definition. booleans have been covered in the file with examples and definitions. Logical comparissons and other ways to sort through data or to make it fluid will expose why we're understanding these types in python.

--- Object-Data-Structures/Sets-Tuples-Booleans.py
+++ Object-Data-Structures/Sets-Tuples-Booleans.py
@@ -3,11 +3,15 @@ Tuples - Are very similar to lists. However they have one key difference - immut
 Once an element is inside a tuple, it can not be reassigned.
 Tuples use parentheses: (1,2,3)
 
-Sets - 
+Sets - Are unordered collection of unique elements. 
+Meaning there can only be one representative of the same object.
+Unique values!
 
-Booleans - 
+Booleans - Are operators that allow you to convey True or False statements.
+These are very important later on when we deal with control flow and logic.
 
 '''
+#TUPLES ###
 # t the tuple
 t = (1,2,3)
 print(type(t)) # return <class 'tuple'>
@@ -37,5 +41,35 @@ print(mylist) # returns - ['One', 2, 3, 4, 5] # no problem! lets try this with a
 Arises the question of why would I use tuples? As a beginner you won't use them much but after becoming more advanced you'll see 
 the importance of their data-integrity and being able to flow throughout a program without having their values reassigned. 
 '''
+# SETS ###
+myset = set()
+print(myset) # returns set()
+myset.add(1)
+print(myset) # returns {1}
+myset.add(2) 
+print(myset) # returns {1, 2}
+#now if you try to add(2) again to myset. you'll recieve the same object as before
+# that is because sets only work with unique values.
+# so any data that is similiar or repetitive are not good for sets.
+# what they are good for are casting unique values from a list example below
+l = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4]
+print(set(l)) # returns {1, 2, 3, 4}
+# be mindful that sets don't have a particular order to them.
+
+#BOOLEANS ###
+print(True) # returns - True
+print(type(True)) # returns - <class 'bool'>
+print(False) # returns - False
+print(type(False)) # returns - <class 'bool'>
+
+print(1 > 2) # False
+print("Yes" == "No") # False
+a = 3
+b = 3
+print(a == b) # True
+# Can use NONE as a placeholder for a boolean value if you want to wait to replace its value with something later on.
+c = None
+print(c) # None
+
 
 
