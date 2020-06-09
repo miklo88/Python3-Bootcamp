516d5cd579eeda9bf7ac0f2c3e791429d135fe9e
Carl Redding <credding07@gmail.com>
2020-06-09 09:27:37 -0400

    created python-statements folder and If-Elif-Else file.

---
+++ Python-Statements/If-Elif-Else.py
@@ -0,0 +1,5 @@
+'''
+Control Flow
+Often only want certain code to execute when a particular condition has been met.
+Example. If my dog is hungry (some condition), then I will feed the dog (some action).
+'''
