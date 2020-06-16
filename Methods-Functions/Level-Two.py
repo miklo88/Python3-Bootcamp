e576c70201cb0d454cfd54a77e943c543816abe8
Carl Redding <credding07@gmail.com>
2020-06-16 13:05:31 -0400

    separated level one practice exercises from function exercise file. moving onto challenge problems

--- Methods-Functions/Level-Two.py
+++ Methods-Functions/Level-Two.py
@@ -71,6 +71,24 @@ print(blackjack(9,9,11))
 #summer_69([1,3,5]) -->9
 #summer_69([4,5,6,7,8,9]) -->9
 #summer_69([2,1,6,9,11]) -->14
+def summer_69(arr):
+    total = 0
+    add = True
+    
+    for num in arr:
+        while add:
+            if num!= 6:
+                total += num
+                break
+            else:
+                add = False
+        while not add:
+            if num != 9:
+                break
+            else:
+                add = True
+                break
+    return total
 #check
 print(summer_69([1,3,5]))
 #check
