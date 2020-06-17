33e3f2ad8d18ccfb7936f8ecd92f2a394b4de575
Carl Redding <credding07@gmail.com>
2020-06-17 16:26:04 -0400

    completed mini game  for one list to be populated with different choices.

--- Milestone1.py/Tic-Tac-Toe.py
+++ Milestone1.py/Tic-Tac-Toe.py
@@ -50,27 +50,72 @@ implemented an input and converted its string input data type into another type 
 now further validating the user input to avoid errors for invalid conversions 
 '''
 
-#using while loop to check input 
-def user_choice():
-    #initial values we need to check
-    choice = 'Incorrect choice' #if it is the correct input type
-    accepted_values = range(0,10) #if the input is within our range
-    within_range = False 
-    #conditions to check.(2) digit or within range == false 
-    #while choice is not a digit we keep asking for the correct digit.
-    while choice.isdigit()==False or within_range==False:
-        choice = input("Enter a number from 0-10: ")
-        #digit check for data type
-        if choice.isdigit() == False:
-            print("That is not the correct digit ya goofball!")
-        #range check for acceptable value
-        if choice.isdigit() == True:
-            if int(choice) in accepted_values:
-                within_range = True
-            else:
-                print("Nope. You're outside of 0 to 10.")
-                within_range = False
-    return int(choice) 
+# #using while loop to check input 
+# def user_choice():
+#     #initial values we need to check
+#     choice = 'Incorrect choice' #if it is the correct input type
+#     accepted_values = range(0,10) #if the input is within our range
+#     within_range = False 
+#     #conditions to check.(2) digit or within range == false 
+#     #while choice is not a digit we keep asking for the correct digit.
+#     while choice.isdigit()==False or within_range==False:
+#         choice = input("Enter a number from 0-10: ")
+#         #digit check for data type
+#         if choice.isdigit() == False:
+#             print("That is not the correct digit ya goofball!")
+#         #range check for acceptable value
+#         if choice.isdigit() == True:
+#             if int(choice) in accepted_values:
+#                 within_range = True
+#             else:
+#                 print("Nope. You're outside of 0 to 10.")
+#                 within_range = False
+#     return int(choice) 
 
-print(user_choice())
+# print(user_choice())
+'''
+now creating a game
+'''
+#creating game list
+game_list = [0,1,2]
+def display_game(game_list):
+    print("Here is the current list")
+    print(game_list)
+display_game(game_list)
+#creating position choice for the user
+def position_choice():
+    choice = 'Wrong'
+    while choice not in ['0','1','2']:
+        choice = input("Pick a position 0,1,2: ")
+        if choice not in ['0','1','2']:
+            print("Invalid choice")
+    return int(choice)
+    #choose a replacement value
+def replacement_choice(game_list,position):
+    user_placement = input("Type something to place at a position: ")
+    game_list[position] = user_placement
+    return game_list
+replacement_choice(game_list,1)
+#now keep the user playing
+def gameon_choice():
+    choice = 'Wrong'
+    while choice not in ['Y', 'N']:
+        choice = input("Keep playing Y or N: ")
+        if choice not in ['Y', 'N']:
+            print("No comprendes, choose Y or N")
+    if choice == "Y":
+        return True
+    else:
+        return False
+
+game_on = True
+game_list = [0,1,2]
+
+while game_on:
+    display_game(game_list)
+    position = position_choice()
+    game_list = replacement_choice(game_list,position)
+    display_game(game_list)
+    game_on = gameon_choice()
+    # return int(choice)
 
