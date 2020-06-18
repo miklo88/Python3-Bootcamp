31a58a3624386e6dcb5954ace61902466bbf8173
Carl Redding <credding07@gmail.com>
2020-06-18 11:10:48 -0400

    set up UPER to talk type my way through the process of creating this application. always humbling to know what I really dont know still.

---
+++ Milestone1.py/Project.py
@@ -0,0 +1,27 @@
+'''
+First Milestone Project.
+Tic Tac Toe for 2 players
+
+1)2 players should be able to play the game
+(Both sitting at the same computer)
+2)The board should be printed out every time a player makes a move
+3)You should be able to accept input of the player position and then place a symbol on the board.
+
+lets break these down into smaller problems
+-quick rundown
+use the numpad to match numbers to the grid on a tic tac toe board:
+[789]
+[456]
+[123]
+this is the layout for our x's and o's
+
+how do i get a user to understand what number to input for their x or o input.
+check to see if someone has won or tied.
+
+##quick peek at game
+on start
+player 1: do you want to be x or o?
+think of battleship for player input.
+Do you want to play again? yes or no? If yes, play again, if no show messages for win, loss, or not finished.
+##
+'''
